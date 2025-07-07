ed181e5c4ef1e3706aa076423f2f7fd3ac053bd8
Dinesh Chinthakuntla <dineshchinthakuntla@gmail.com>
2025-07-07 14:47:02 +0530

    bitwise_operators

---
+++ bitwise.py
@@ -0,0 +1,16 @@
+# and bitwise operator
+
+a=96
+b=63 
+print(bin(a))
+print(bin(b))
+
+print(bin(a & b))
+print(a & b)
+print(bin(a | b))
+print(a | b)
+
+
+
+
+
---
+++ or_bitwise_operator.py
@@ -0,0 +1,23 @@
+# or bitwise operator
+
+a=96
+b=62
+print(bin(a))
+print(bin(b))
+
+print(bin(a | b))
+print(a | b)
+
+
+'''0b1111110'''
+
+
+							
+							
+							
+							
+							
+							
+							
+							
+
